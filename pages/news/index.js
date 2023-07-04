cfdc501c2850009de0c645075a8fcd141fafaad3
Prach Ramesh Godase <prachigodase2@gmail.com>
2023-07-04 19:10:37 +0530

    Making Nested Routes

---
+++ pages/news/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//our-domian.com/news
+const NewsPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Next.js index page!</h1>
+    </div>
+  );
+};
+
+export default  NewsPage;
---
+++ pages/news/something-important.js
@@ -0,0 +1,11 @@
+import React from 'react';
+//our-domian.com/news/something-important
+const DetailPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Next.js Detail page!</h1>
+    </div>
+  );
+};
+
+export default  DetailPage;
